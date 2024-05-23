f89643843096468bc88706d84595a252c8a48d0c
rorymclaughlin <rmclaughlin432@gmail.com>
2024-05-23 11:48:41 +0100

    update index

--- api/index.js
+++ api/index.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const cors = require("cors");
 const usercollection = require("./routes/usercollection.js");
+const bodyParser = require("body-parser");
 
 const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", usercollection);
 
 // start the Express server
