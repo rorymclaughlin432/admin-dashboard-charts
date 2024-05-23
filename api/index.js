2750d71d0516b3e11b52ab27521cfed68eed64a1
rorymclaughlin <rmclaughlin432@gmail.com>
2024-05-23 11:50:47 +0100

    serve

--- api/index.js
+++ api/index.js
@@ -7,7 +7,8 @@ const port = process.env.PORT || 3000;
 const app = express();
 
 app.use(cors());
-app.use(bodyParser.json()); app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/", usercollection);
 
 // start the Express server
